da5f34f459a6e0ff94b532b5790065055af08429
booringreader <157519312+booringreader@users.noreply.github.com>
2026-02-21 19:36:56 +0530

    fix2: vite-favicon package

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,10 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  const vitePluginFaviconsInject = require('vite-plugin-favicons-inject');
   return {
     plugins: [
       react(), 
       tailwindcss(),
-      vitePluginFaviconsInject,
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
